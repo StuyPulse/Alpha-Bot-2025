29c47e58c7463e7301e9ceeed9b4364050243e88
Lucas <lucas.cs.programming@gmail.com>
2025-01-17 17:43:29 -0500

    remove sim vision and other unneeded methods

--- src/main/java/com/stuypulse/robot/subsystems/vision/AprilTagVision.java
+++ src/main/java/com/stuypulse/robot/subsystems/vision/AprilTagVision.java
@@ -6,24 +6,7 @@ import com.stuypulse.robot.Robot;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class AprilTagVision extends SubsystemBase {
-
-    private static final AprilTagVision instance;
-
-    static {
-        if (Robot.isReal()) {
-            instance = new LimelightVision();
-        }
-        else {
-            instance = new SimVision();
-        }
-    }
-
-    public static AprilTagVision getInstance() {
-        return instance;
-    }
-
     public abstract void setTagWhitelist(int... ids);
-
     public abstract void setCameraEnabled(String name, boolean enabled);
 
     public abstract void disable();
