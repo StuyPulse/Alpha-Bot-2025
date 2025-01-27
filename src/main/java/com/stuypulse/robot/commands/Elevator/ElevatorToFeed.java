0d1a2ce76fcaf75692ed6cd9389a8eab965ca6bf
Ian Shi <69531533+IanShiii@users.noreply.github.com>
2025-01-26 20:53:31 -0500

    Delete src/main/java/com/stuypulse/robot/commands/Elevator directory

--- src/main/java/com/stuypulse/robot/commands/Elevator/ElevatorToFeed.java
+++
@@ -1,9 +0,0 @@
-package com.stuypulse.robot.commands.elevator;
-
-import com.stuypulse.robot.constants.Settings.Elevator;
-
-public class ElevatorToFeed extends ElevatorToHeight{
-    public ElevatorToFeed(){
-        super(Elevator.FEED_HEIGHT_METERS);
-    }
-}
